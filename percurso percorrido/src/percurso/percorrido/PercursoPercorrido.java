87982e738ac6ee10047801db01ad3d126daf0f9f
Brenda Cristina <65242957+brendacristina@users.noreply.github.com>
2020-05-20 14:44:11 -0300

    lista de exercicios fluxograma

---
+++ percurso percorrido/src/percurso/percorrido/PercursoPercorrido.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percurso.percorrido;
+
+/**
+ *
+ * @author noot
+ */
+public class PercursoPercorrido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ valores dos numeros/src/valores/dos/numeros/ValoresDosNumeros.java
@@ -0,0 +1,39 @@
+
+
+package valores.dos.numeros;
+
+import java.util.Scanner;
+
+
+public class ValoresDosNumeros {
+     //deinição de variaveis   
+    double n1, n2, n3 ,n4 ,valor ;
+    
+    //entrada
+       
+    Scanner entrada = new Scanner(System.in);
+            
+            System.out.println("qual o valor do primeiro numero");
+            n1 =entada.nexDouble();
+            
+            System.out.println("qual o valor do segundo numero");
+            n2 =entrada.nexDouble();
+            
+            System.out.println("qual o valor do terceiro numero");
+            n3 =entrada.nextDouble();
+            
+            System.out.println("qual o valor do quarto numero");
+            n4 =entrada.nexDouble();
+            
+            //processamento
+            
+            valor =(n1^1)+(n2^2)+(n3^3)+(n4^2);
+            
+            //saida
+            
+            System.out.println("a quantidade sera"+valor)
+    
+    
+    }
+    
+}
