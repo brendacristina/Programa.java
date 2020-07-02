cbe2e51b361193400c3042a0887279fd4d3ca903
Brenda Cristina <65242957+brendacristina@users.noreply.github.com>
2020-07-02 17:42:15 -0300

    Add files via upload

---
+++ ImparOuPar.java
@@ -0,0 +1,23 @@
+
+package impar.ou.par;
+
+
+public class ImparOuPar {
+
+    public static void main(String[] args) {
+       int numero;
+
+
+
+  numero=Double.parseDouble(JOptionPane.showInputDialog;("digite um valor"))
+
+ if(num mod 2)=0{
+     JOptionPane.showMessageDialog(null, "o numero digitado é par ");
+    
+    
+    
+    }else 
+   JOptionPane.showMessageDialog(null, "o numero digitado é impar ");
+    }
+    
+}
